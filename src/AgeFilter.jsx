a2e680a5e1ef075319c1008c46bbc850ed6c5db2
urbanseabear <calvin.rudy@gmail.com>
2021-01-29 14:26:48 -0500

    minor style changes to filters

--- src/AgeFilter.jsx
+++ src/AgeFilter.jsx
@@ -8,6 +8,7 @@ const AgeFilter = ({ handleChange }) => {
       <label htmlFor="ageLow">Min. Age</label>
       <input type="number" id="ageLow" name="ageLow" min="1" max="200" onChange={(e) => handleChange(e)}></input>
       </div>
+      
       <div className="age-in">
       <label htmlFor="ageHi">Max Age</label>
       <input type="number" id="ageHi" name="ageHi" min="1" max="200" onChange={(e) => handleChange(e)}></input>
